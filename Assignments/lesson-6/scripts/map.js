4e7f60902ba278617c3639174f1593baa6ac1de1
jgoudie18003 <gou18003@byui.edu>
2019-02-18 22:13:17 -0500

    update

--- Assignments/lesson-6/scripts/map.js
+++ Assignments/lesson-6/scripts/map.js
@@ -4,5 +4,5 @@ function myMap() {
         center:new google.maps.LatLng(42.1111605, -111.8410790),
         zoom: 12,
     };
-    var map = new google.maps.MAP(document.getElementById("googlemap"), mapProp)
+    var map = new google.maps.MAP(document.getElementById("googleMap"), mapProp)
 }
