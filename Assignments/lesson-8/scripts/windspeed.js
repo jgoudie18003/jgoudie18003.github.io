ccaf2e5afd87c9e6310aa6ea1e58ab1f65d1fdff
jgoudie18003 <gou18003@byui.edu>
2019-03-04 18:31:02 -0500

    update

--- Assignments/lesson-8/scripts/windspeed.js
+++ Assignments/lesson-8/scripts/windspeed.js
@@ -1,9 +1,8 @@
     var tempF = parseInt(document.getElementById("tempF").innerHTML);
-    var speed = parseInt(document.getElementById("speed").innerHTML);
-    var result = windChill(tempF, speed); 
+    var speed = parseInt(document.getElementById("speed").innerHTML); 
     
     document.getElementById("outputDiv").innerHTML = "The Wind Chill Feels Like " + result + "°F";
     
     var windspeed = Math.pow(speed, 0.16);
-    var feelslike = 35.74 + (0.6215 * tempF) - (35.75 * windspeed) + (0.4275 * tempF * windspeed);
-    return feelslike.toFixed (0);
+    var outputDiv = 35.74 + (0.6215 * tempF) - (35.75 * windspeed) + (0.4275 * tempF * windspeed);
+    return outputDiv.toFixed (0);
