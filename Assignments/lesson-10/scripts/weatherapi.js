59fc6ded2534e2ab969423f4bb18ff697b79a457
jgoudie18003 <gou18003@byui.edu>
2019-03-15 19:56:37 -0400

    update

--- Assignments/lesson-10/scripts/weatherapi.js
+++ Assignments/lesson-10/scripts/weatherapi.js
@@ -2,15 +2,14 @@
 var requestURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=1712458fe634bfb44de4aeb2b8175957";
 var request = new XMLHttpRequest();
 request.open('GET', requestURL);
-request.responseType = 'json';
 request.send();
 
-request.onload =  function () {
+request.onload = function () {
     let weatherData = JSON.parse(request.responseText);
-
+    console.log(weatherData);
     var tempF = weatherData.main.temp;
-    var speed = weatherData.wind.speed; 
-    
+    var speed = weatherData.wind.speed;
+
     var windspeed = Math.pow(speed, 0.16);
     var outputDiv = 35.74 + 0.6215 * tempF - 35.75 * windspeed + 0.4275 * tempF * windspeed;
     outputDiv = Math.round(outputDiv);
