b2a0c5ad2412869e4bef7e0e1a43358d5475e75d
jgoudie18003 <gou18003@byui.edu>
2019-03-15 19:44:35 -0400

    update

--- Assignments/lesson-10/scripts/weatherapi.js
+++ Assignments/lesson-10/scripts/weatherapi.js
@@ -1,3 +1,4 @@
+
 var section = document.getElementById("weatherapi");
 var requestURL = "https://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={1712458fe634bfb44de4aeb2b8175957}";
 var request = new XMLHttpRequest();
@@ -8,45 +9,13 @@ weatherRequest.send();
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
 
-{
-    "coord": {
-        "lon": 145.77,
-            "lat": -16.92
-    },
-    "weather": [
-        {
-            "id": 802,
-            "main": "Clouds",
-            "description": "scattered clouds",
-            "icon": "03n"
-        }
-    ],
-        "base": "stations",
-            "main": {
-        "temp": 300.15,
-            "pressure": 1007,
-                "humidity": 74,
-                    "temp_min": 300.15,
-                        "temp_max": 300.15
-    },
-    "visibility": 10000,
-        "wind": {
-        "speed": 3.6,
-            "deg": 160
-    },
-    "clouds": {
-        "all": 40
-    },
-    "dt": 1485790200,
-        "sys": {
-        "type": 1,
-            "id": 8166,
-                "message": 0.2064,
-                    "country": "AU",
-                        "sunrise": 1485720272,
-                            "sunset": 1485766550
-    },
-    "id": 2172797,
-        "name": "Cairns",
-            "cod": 200
+    var tempF = weatherData.main.temp;
+    var speed = weatherData.wind.speed; 
+    
+    var windspeed = Math.pow(speed, 0.16);
+    var outputDiv = 35.74 + 0.6215 * tempF - 35.75 * windspeed + 0.4275 * tempF * windspeed;
+    outputDiv = Math.round(outputDiv);
+
+    document.getElementById("outputDiv").innerHTML = outputDiv + "°F";
+
 }
