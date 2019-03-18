2f38e2f513f946c448d150f1c991f90c65a3a5ba
jgoudie18003 <gou18003@byui.edu>
2019-03-17 22:48:04 -0400

    update

--- Assignments/lesson-10/scripts/eventdata.js
+++ Assignments/lesson-10/scripts/eventdata.js
@@ -5,40 +5,40 @@ eventRequest.responseType = "json";
 eventRequest.send();
 
 eventRequest.onload = function () {
-    
-    var info = eventRequest.response;
-    showPrestonEvents(info);
+
+  var info = eventRequest.response;
+  showPrestonEvents(info);
 }
 
 function showPrestonEvents(info) {
 
-    var eventIndex = 0;
-    var eventList = info["towns"];
-    
-    for (eventIndex=0; eventIndex < eventList.length; eventIndex++) {
-        
-        if (eventList[eventIndex].name == "Preston") {
-            
-            var eventArticle = document.createElement("article");
-            var listed = document.createElement("ol");
-            
-            
-            var events = eventList[eventIndex].events;
-            
-            for (i=0; i < events.length; i++) {
-                
-                var eventLine = document.createElement("li");
-                eventLine.textContent = events[i];
-                listed.appendChild(eventLine);
-                
-            }
-            eventArticle.appendChild(listed);
-            eventsPreston.appendChild(eventArticle);
-            
-        }
-        
+  var eventIndex = 0;
+  var eventList = info["towns"];
+
+  for (eventIndex = 0; eventIndex < eventList.length; eventIndex++) {
+
+    if (eventList[eventIndex].name == "Preston") {
+
+      var eventArticle = document.createElement("article");
+      var listed = document.createElement("ol");
+
+
+      var events = eventList[eventIndex].events;
+
+      for (i = 0; i < events.length; i++) {
+
+        var eventLine = document.createElement("li");
+        eventLine.textContent = events[i];
+        listed.appendChild(eventLine);
+
+      }
+      eventArticle.appendChild(listed);
+      eventsPreston.appendChild(eventArticle);
+
     }
-    
+
+  }
+
 }
 
 var eventsPreston = document.querySelector("townEvents");
