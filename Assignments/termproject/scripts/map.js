4f4ff6ae7f76177288a7d5f89d7d5c56c4f18a03
jgoudie18003 <gou18003@byui.edu>
2019-04-05 11:09:47 -0400

    update

--- Assignments/termproject/scripts/map.js
+++ Assignments/termproject/scripts/map.js
@@ -1,7 +1,7 @@
 /*Map for Bethesda, Maryland*/
 function myMap() {
     var mapProp = {
-        center:new google.maps.LatLng(38.9847, 77.0947),
+        center:new google.maps.LatLng(38.9816047, -77.1535963),
         zoom: 12,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
