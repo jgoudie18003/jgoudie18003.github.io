aae0a699d80a728331b7ee78aec250632e14c077
jgoudie18003 <gou18003@byui.edu>
2019-04-05 10:51:11 -0400

    update

--- Assignments/termproject/scripts/map.js
+++ Assignments/termproject/scripts/map.js
@@ -1,7 +1,7 @@
-/*Map for Preston*/
+/*Map for Bethesda, Maryland*/
 function myMap() {
     var mapProp = {
-        center:new google.maps.LatLng(42.1111605, -111.8410790),
+        center:new google.maps.LatLng(38.9847, 77.0947),
         zoom: 12,
     };
     var map = new google.maps.Map(document.getElementById("googleMap"), mapProp)
